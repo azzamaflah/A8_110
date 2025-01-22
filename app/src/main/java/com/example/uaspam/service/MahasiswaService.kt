5ac67d4251ca79f3a70614edf8852096ff876d06
azzamaflah <azzamhebat46@gmail.com>
2025-01-22 22:45:50 +0700

    membuat mahasiswa service agar fungsi" nya nanti bisa di panggil di repository

---
+++ app/src/main/java/com/example/uaspam/service/MahasiswaService.kt
@@ -0,0 +1,34 @@
+package com.example.uaspam.service
+
+import com.example.uaspam.model.Mahasiswa
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
+
+interface MahasiswaService {
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json"
+    )
+    @GET("bacamahasiswa.php")
+    suspend fun getMahasiswa(): List<Mahasiswa>
+
+    @GET("bacamahasiswaid.php/{id_mahasiswa}")
+    suspend fun getMahasiswaById(@Query("id_mahasiswa") id_mahasiswa:String):Mahasiswa
+
+    @POST("insertmahasiswa.php")
+    suspend fun insertMahasiswa(@Body mahasiswa: Mahasiswa)
+
+    @PUT("editmahasiswa.php")
+    suspend fun updateMahasiswa(@Query("id_mahasiswa")id_mahasiswa: String, @Body mahasiswa: Mahasiswa)
+
+    @DELETE("deletemahasiswa.php/{id_mahasiswa}")
+    suspend fun deleteMahasiswa(@Query("id_mahasiswa")id_mahasiswa: String): Response<Void>
+
+
+}
