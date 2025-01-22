1dc4cdebb8f52c7ab8abcdfd879307faf15459b5
azzamaflah <azzamhebat46@gmail.com>
2025-01-22 23:36:19 +0700

    untuk mengelola data mahasiswa dengan metode CRUD

--- app/src/main/java/com/example/uaspam/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/uaspam/repository/MahasiswaRepository.kt
@@ -1,2 +1,44 @@
 package com.example.uaspam.repository
 
+import com.example.uaspam.model.Mahasiswa
+import com.example.uaspam.service.MahasiswaService
+import java.io.IOException
+
+interface MahasiswaRepository {
+    suspend fun getMahasiswa(): List<Mahasiswa>
+    suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+    suspend fun updateMahasiswa(idMahasiswa: String, mahasiswa: Mahasiswa)
+    suspend fun deleteMahasiswa(idMahasiswa: String)
+    suspend fun getMahasiswaById(idMahasiswa: String): Mahasiswa
+}
+
+class NetworkMahasiswaRepository(
+    private val mahasiswaApiService: MahasiswaService
+) : MahasiswaRepository {
+    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
+        mahasiswaApiService.insertMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMahasiswa(idMahasiswa: String, mahasiswa: Mahasiswa) {
+        mahasiswaApiService.updateMahasiswa(idMahasiswa, mahasiswa)
+    }
+
+    override suspend fun deleteMahasiswa(idMahasiswa: String) {
+        try {
+            val response = mahasiswaApiService.deleteMahasiswa(idMahasiswa)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete mahasiswa. HTTP Status code: ${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getMahasiswa(): List<Mahasiswa> = mahasiswaApiService.getMahasiswa()
+    override suspend fun getMahasiswaById(idMahasiswa: String): Mahasiswa {
+        return mahasiswaApiService.getMahasiswaById("idMahasiswa")
+    }
+}
